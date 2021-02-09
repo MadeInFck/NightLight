c7617be334014a117d638b73be73792dc24b8866
MadeInFck <webmaster@madeinfck.com>
2021-02-09 09:27:40 +0100

    code cleaning + comments

--- NightLight.ino
+++ NightLight.ino
@@ -1,141 +1,52 @@
-// Bibliothèques
-//#include <TinyWireM.h>   // bibliothèque i2c pour ATTiny
-//#include <TinyRTClib.h>  // bibliothèque horloge temps réel pour ATTiny
+// Libraries
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <RTC.h>
 
+//Leds define parameters: 3 leds, linked ton pin 4
 #define NUMPIXELS 3
 #define PIN       4
 
+//Instantiate RTC & leds
 static DS1307 RTC;
-//RTC_DS1307 RTC;        // initialisation de l'horloge temps réel
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 int blue = 255, green = 255, red = 255;
 
 void setup () {
-
-  //TinyWireM.begin();            // Initialisation I2C
-  //RTC.begin();                  // Initialisation de l'horloge DS1307
+  
+//Start led process & set brigthness
   pixels.begin();
   pixels.setBrightness(30);
- 
-  RTC.begin();
- // RTC.setDateTime(__DATE__, __TIME__);
-  //RTC.startClock();
-  Serial.begin(9600);
-//  Serial.println("New Time Set");
-//  
 
-//  Serial.println();
-//  Serial.println("*** RTC 1307 ***");
-//  Serial.print("Is Clock Running : ");
-//  if (RTC.isRunning())
-//    Serial.println("Yes");
-//  else
-//    Serial.println("No. Time may be Inaccurate");
-//  Serial.print("Hour Mode : ");
-//  if (RTC.getHourMode() == CLOCK_H24)
-//    Serial.println("24 Hours");
-//  else
-//    Serial.println("12 Hours");
-//  Serial.print("Is Out Pin Enabled : ");
-//  if (RTC.isOutPinEnabled())
-//    Serial.println("Yes");
-//  else
-//    Serial.println("No");
-//
-//  Serial.print("Is SQWE Enabled : ");
-//  if (RTC.isSqweEnabled())
-//    Serial.println("Yes");
-//  else
-//    Serial.println("No");
-//
-//   switch (RTC.getWeek())
-//  {
-//    case 1:
-//      Serial.print("SUN");
-//      break;
-//    case 2:
-//      Serial.print("MON");
-//      break;
-//    case 3:
-//      Serial.print("TUE");
-//      break;
-//    case 4:
-//      Serial.print("WED");
-//      break;
-//    case 5:
-//      Serial.print("THU");
-//      break;
-//    case 6:
-//      Serial.print("FRI");
-//      break;
-//    case 7:
-//      Serial.print("SAT");
-//      break;
-//  }
-//  
-//  Serial.print(" ");
-//  Serial.print(RTC.getDay());
-//  Serial.print("-");
-//  Serial.print(RTC.getMonth());
-//  Serial.print("-");
-//  Serial.print(RTC.getYear());
-//
-//  Serial.print(" ");
-//
-//  Serial.print(RTC.getHours());
-//  Serial.print(":");
-//  Serial.print(RTC.getMinutes());
-//  Serial.print(":");
-//  Serial.print(RTC.getSeconds());
-//  
-//  if (RTC.getHourMode() == CLOCK_H12)
-//  {
-//    switch (RTC.getMeridiem())
-//    {
-//      case HOUR_AM :
-//        Serial.print(" AM");
-//        break;
-//      case HOUR_PM :
-//        Serial.print(" PM");
-//        break;
-//    }
-//  }
-//  Serial.println();
+//Start RTC & serial 
+  RTC.begin();
+  Serial.begin(9600);
+  
 }
 
 void loop () {
-
+//Conditions to switch light according to daytime
 if (RTC.getHours() >= 20 || RTC.getHours() < 7) {
   blue=255;
   red=0;
   green=0; 
-} else if (RTC.getHours() >= 7 && RTC.getHours() < 9) {
+} else if (RTC.getHours() >= 7 && RTC.getMinutes() < 30) {
   blue=0;
   red=0;
-  green=255;
-} else if (RTC.getHours() >= 9 && RTC.getHours() < 18){
-  blue=0;
-  red=255;
   green=255;
 } else {
   blue=255;
   red=255;
   green=255;
 }
-
+//Display led
   for(int i=0;i<NUMPIXELS;i++){
-
       pixels.setPixelColor(i, pixels.Color(red,green,blue));
-
       pixels.show(); // This sends the updated pixel color to the hardware.
-
-     
   }
+
+ //Serial logging to check the code what's going on
   Serial.print("Rouge: ");
   Serial.print(red);
   Serial.print(" Vert: ");
